5032096e23360bd3cd7d965fd1238c0392e7ab52
Kamran Liaquat <itskamranliaquat@gmail.com>
2026-03-31 03:05:22 +0500

    Update crackTime.js

--- crackTime.js
+++ crackTime.js
@@ -1,11 +1,23 @@
 function estimateCrackTime(password) {
     const charset = 94;
-    const possibilities = Math.pow(charset, password.length);
     const guessesPerSecond = 1e9;
-    const seconds = possibilities / guessesPerSecond;
-    const years = seconds / (60*60*24*365);
 
-    if (years < 0.01) return "<1 week";
-    if (years < 1) return "<1 year";
-    return years.toFixed(0) + " years";
+    const combinations = Math.pow(charset, password.length);
+    let seconds = combinations / guessesPerSecond;
+
+    const minute = 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+    const week = day * 7;
+    const year = day * 365;
+
+    if (seconds < minute) return "Few seconds";
+    if (seconds < hour) return Math.floor(seconds / minute) + " minutes";
+    if (seconds < day) return Math.floor(seconds / hour) + " hours";
+    if (seconds < week) return Math.floor(seconds / day) + " days";
+    if (seconds < year) return Math.floor(seconds / week) + " weeks";
+    if (seconds < year * 10) return Math.floor(seconds / year) + " years";
+    if (seconds < year * 100) return Math.floor(seconds / year) + " years";
+    
+    return "100+ years";
 }
